fc25cc1116ee63a44977421e6280ff33b4c9417b
kongyulu <kongyl@rd.wondershare.cn>
2020-07-11 14:40:10 +0800

    完善服务器代码

--- demos/ios/kp2p_demo/KSDPServer_swift/KSDPServer_swift/ViewController.swift
+++ demos/ios/kp2p_demo/KSDPServer_swift/KSDPServer_swift/ViewController.swift
@@ -12,9 +12,17 @@ class ViewController: NSViewController {
     
     private var server:KWebSocketServer?
 
+    @IBOutlet weak var scrollView: NSScrollView!
+    @IBOutlet var textView: NSTextView!
+    
+    @IBOutlet weak var btnStart: NSButton!
+    private var bStarted: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         server = try? KWebSocketServer()
+        server?.delegate = self
+        btnStart.title = "启动"
         // Do any additional setup after loading the view.
     }
 
@@ -29,8 +37,24 @@ class ViewController: NSViewController {
             print("没有创建server")
             return
         }
-        server.start()
+        if bStarted {
+            server.stop()
+            btnStart.title = "启动"
+        } else {
+            server.start()
+            btnStart.title = "停止"
+        }
+
+        bStarted = !bStarted
     }
-    
+}
+
+extension ViewController:KWebSocketServerMsgDelegate {
+    func didReceive(msg: String) {
+        DispatchQueue.main.async {
+            let message = self.textView.string + "\n" + "\(Date()):\(msg)"
+            self.textView.string = message
+        }
+    }
 }
 
--- demos/ios/kp2p_demo/KSDPServer_swift/KSDPServer_swift/server/KWebSocketServer.swift
+++ demos/ios/kp2p_demo/KSDPServer_swift/KSDPServer_swift/server/KWebSocketServer.swift
@@ -9,11 +9,19 @@
 import Cocoa
 import Network
 
+protocol KWebSocketServerMsgDelegate {
+    func didReceive(msg:String)
+}
+
 class KWebSocketServer {
     
     private let queue = DispatchQueue.global()
     private let port : NWEndpoint.Port = 8080
     private let listener: NWListener
+    private var state:NWListener.State = .cancelled
+    
+    
+    var delegate:KWebSocketServerMsgDelegate?
     
     /// 保存建立连接 的客户端
     private var connectedClients = Set<KWebSocketClient>()
@@ -33,16 +41,33 @@ class KWebSocketServer {
     
     //启动服务
     func start() {
+        guard state != .ready, state != .setup else {
+            let msg = "启动监听端口：\(port) 失败，state = \(state)"
+            print(msg)
+            delegate?.didReceive(msg: msg)
+            return
+        }
         //设置接收到消息的代理回调
         listener.newConnectionHandler = newConnectionHandler(_:)
+        //设置状态回调
+        listener.stateUpdateHandler = stateUpdateHandler(_:)
         //开启服务器后台监听
         listener.start(queue: queue)
-        print("启动成功信令服务器，端口：\(port)")
+        let msg = "启动监听端口：\(port)"
+        print(msg)
+        delegate?.didReceive(msg: msg)
+    }
+    
+    func stop() {
+        listener.cancel()
+        let msg = "停止监听端口：\(port)"
+        print(msg)
+        delegate?.didReceive(msg: msg)
     }
 
 }
 
-// MARK: - 连接代理
+// MARK: - 发送消息
 fileprivate extension KWebSocketServer {
     func send(msg: Data, to client:KWebSocketClient) {
         //设置元数据为二进制数据
@@ -50,7 +75,9 @@ fileprivate extension KWebSocketServer {
         //创建一个连接上下文
         let context = NWConnection.ContentContext(identifier: "kyl_server_context", expiration: 120, priority: 1.0, isFinal: true, antecedent: nil, metadata: [metadata])
         client.connection.send(content: msg, contentContext: context, isComplete: true, completion: .contentProcessed({ (err) in
-            print("contentProcessed, err=\(String(describing: err))")
+            let msg = "contentProcessed, err=\(String(describing: err))"
+            print(msg)
+            self.delegate?.didReceive(msg: msg)
         }))
     }
     
@@ -99,6 +126,7 @@ fileprivate extension KWebSocketServer {
             _ = broadcast(msg: data, to: others)
             if let msg = String(data: data, encoding: .utf8) {
                 print("****收到消息：\(msg) \n")
+                delegate?.didReceive(msg: msg)
             }
         }
         
@@ -111,6 +139,21 @@ fileprivate extension KWebSocketServer {
     
     func didDisconnected(client: KWebSocketClient) {
         connectedClients.remove(client)
-        print("收到客户端端口连接，当前总连接数量：\(connectedClients.count)")
+        let msg = "收到客户端端口连接，当前总连接数量：\(connectedClients.count)"
+        print(msg)
+        delegate?.didReceive(msg: msg)
+    }
+}
+
+// MARK: - 状态代理
+fileprivate extension KWebSocketServer {
+    /// Set a block to be called when the listener's state changes, which may be called
+    /// multiple times until the listener is cancelled.
+    //final public var stateUpdateHandler: ((NWListener.State) -> Void)?
+    func stateUpdateHandler(_ state:NWListener.State) -> Void {
+        self.state = state
+        let msg = "监听状态变化：\(state)"
+        print(msg)
+        delegate?.didReceive(msg: msg)
     }
 }
